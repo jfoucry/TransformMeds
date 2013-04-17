1aaa959d59490f5080ffc7597e455a9d56dc4f0e
Jacques Foucry <jacques@foucry.net>
2013-04-17 13:38:21 +0200

    Commit initial

---
+++ test-meds/main.m
@@ -0,0 +1,31 @@
+//
+//  main.m
+//  test-meds
+//
+//  Created by Jacques Foucry on 16/04/13.
+//  Copyright (c) 2013 Jacques Foucry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+		NSArray *oldMedsArray = [NSArray arrayWithContentsOfFile:@"/tmp/meds.plist"];
+		
+		NSMutableDictionary *medsDict = [[NSMutableDictionary alloc] init];
+		for (NSDictionary *aDict in oldMedsArray) {
+			NSString *key = [aDict objectForKey:@"cip7"];
+			NSLog(@"key ==%@",key);
+			[medsDict setObject:aDict forKey:key];
+		}
+		
+		[medsDict writeToFile:@"/tmp/Meds-new.plist" atomically:NO];
+	}
+    return 0;
+}
+
