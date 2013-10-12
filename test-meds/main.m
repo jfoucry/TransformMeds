d364de04deb23518f4b8903f555ff2b6f5413197
Jacques Foucry <jacques@foucry.net>
2013-10-12 08:58:53 +0200

    Local change

--- test-meds/main.m
+++ test-meds/main.m
@@ -14,17 +14,18 @@ int main(int argc, const char * argv[])
 	@autoreleasepool {
 	    
 	    // insert code here...
-	    NSLog(@"Hello, World!");
+	    NSLog(@"Start process");
 		NSArray *oldMedsArray = [NSArray arrayWithContentsOfFile:@"/tmp/meds.plist"];
 		
 		NSMutableDictionary *medsDict = [[NSMutableDictionary alloc] init];
 		for (NSDictionary *aDict in oldMedsArray) {
 			NSString *key = [aDict objectForKey:@"cip7"];
-			NSLog(@"key ==%@",key);
+//			NSLog(@"key ==%@",key);
 			[medsDict setObject:aDict forKey:key];
 		}
 		
 		[medsDict writeToFile:@"/tmp/Meds-new.plist" atomically:NO];
+		NSLog(@"End process");
 	}
     return 0;
 }
