e47fcb71523221d00dcdfa0be505bb40057e72d4
Jacques Foucry <jacques@foucry.net>
2014-02-27 15:27:12 +0100

    Essai de gestion des paramètres

--- test-meds/main.m
+++ test-meds/main.m
@@ -13,19 +13,40 @@ int main(int argc, const char * argv[])
 
 	@autoreleasepool {
 	    
-	    // insert code here...
-	    NSLog(@"Start process");
-		NSArray *oldMedsArray = [NSArray arrayWithContentsOfFile:@"/tmp/meds.plist"];
-		
-		NSMutableDictionary *medsDict = [[NSMutableDictionary alloc] init];
-		for (NSDictionary *aDict in oldMedsArray) {
-			NSString *key = [aDict objectForKey:@"cip7"];
-//			NSLog(@"key ==%@",key);
-			[medsDict setObject:aDict forKey:key];
+	    if (argc == 0)
+	    {
+	    	printf("You must provide path to file to transform\n");
+	    	return 255;
+	    } else if (argc > 1)
+	    {
+	    	printf("Too much argument. You just need to provide path to file to transform\n");
+	    	return 255;
+	    }
+	    NSString *filePath = [NSString stringWithUTF8Stringärgv[1]];
+
+	    NSFIleManager *fileManager = [NSFIleManager defaultManager];
+
+	    if ([fileManager fileExistsAtPath:filePath])
+	    {
+	    	/* code */
+	    
+		    NSLog(@"Start process");
+			NSArray *oldMedsArray = [NSArray arrayWithContentsOfFile:filePath];
+			
+			NSMutableDictionary *medsDict = [[NSMutableDictionary alloc] init];
+			for (NSDictionary *aDict in oldMedsArray) {
+				NSString *key = [aDict objectForKey:@"cip7"];
+	//			NSLog(@"key ==%@",key);
+				[medsDict setObject:aDict forKey:key];
+			}
+			
+			[medsDict writeToFile:@"/tmp/Meds-new.plist" atomically:NO];
+			NSLog(@"End process");
 		}
-		
-		[medsDict writeToFile:@"/tmp/Meds-new.plist" atomically:NO];
-		NSLog(@"End process");
+		else {
+			printf("Error, %@ not found\n", filePath);
+			return 255;
+		}
 	}
     return 0;
 }
