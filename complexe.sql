a0cd7f98ad3c90c555f56dba7bf02c154fe0eec2
Jacques Foucry <github@foucry.net>
2022-02-27 11:41:41 +0100

    Add genere_type column in both request (#5)
    Co-authored-by: Jacques Foucry <jacques@foucry.net>

--- complexe.sql
+++ complexe.sql
@@ -1,6 +1,6 @@
 SELECT CIS_CIP.cis,
                        CIS_CIP.cip13,CIS.admin_mode,CIS.nom_court,CIS_CIP.pres,CIS_CIP.cip7,
-                       CIS_GENER.libelle_group FROM CIS
+                       CIS_GENER.libelle_group, CIS_GENER.gener_type FROM CIS
                        INNER JOIN CIS_CIP
                        ON CIS.cis = CIS_CIP.cis
                        LEFT JOIN CIS_GENER
